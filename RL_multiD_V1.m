aa0f366a2b8bf439be6980b30a8d32ccfbf3ad9e
Sabrina Abram <sabram@sfu.ca>
2018-01-16 12:51:30 -0800

    another test with more commenting

--- RL_multiD_V1.m
+++ RL_multiD_V1.m
@@ -1,1 +1,4 @@
 % Created 16 January 2018
+% The purpose of the first version of this code is to combine Max's
+% multi-dimensional reinforcement learning algorithm with recent advances
+% in Jess' single-dimension reinforcement learning algorithm
