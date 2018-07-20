434e8323240a97a07762e82f9ae4c736da9384f0
Sabrina Abram <abramsabrina@gmail.com>
2018-07-20 14:01:41 -0400

    Delete RL_multiD_V1.m

--- RL_multiD_V1.m
+++
@@ -1,4 +0,0 @@
-% Created 16 January 2018
-% The purpose of the first version of this code is to combine Max's
-% multi-dimensional reinforcement learning algorithm with recent advances
-% in Jess' single-dimension reinforcement learning algorithm
